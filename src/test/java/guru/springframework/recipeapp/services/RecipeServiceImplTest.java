df619c9fb41b20f893b8ec2875bfd23d7c1f3d6f
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-21 09:39:37 -0500

    Usando mockito Mocks

--- src/test/java/guru/springframework/recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/recipeapp/services/RecipeServiceImplTest.java
@@ -1,4 +1,44 @@
+package guru.springframework.recipeapp.services;
+
+import guru.springframework.recipeapp.domain.Recipe;
+import guru.springframework.recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class RecipeServiceImplTest {
-  
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet receipeData = new HashSet();
+        receipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
 }
