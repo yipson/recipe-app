0adb8efb7f2b7d2faf533bd9166f99a7461383dd
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-21 15:28:41 -0500

    Aniadiendo maven failse plugin

--- src/test/java/guru/springframework/recipeapp/RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/recipeapp/RecipeAppApplicationTests.java
@@ -1,13 +1,16 @@
 package guru.springframework.recipeapp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class RecipeAppApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
 
 }
