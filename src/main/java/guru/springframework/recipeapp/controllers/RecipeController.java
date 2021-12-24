7490c6fba82f98f20b0e0a9fccb7ac990d00e986
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-24 15:42:06 -0500

    Create a Recipe

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package guru.springframework.recipeapp.controllers;
 
+import guru.springframework.recipeapp.commands.RecipeCommand;
 import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -21,5 +21,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        System.out.println(savedCommand);
+         return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
